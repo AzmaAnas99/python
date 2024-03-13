e88f6c6991ad394b25eb6a12a7e71a8f957ffbb8
AzmaAnas99 <azmaanas05@gmail.com>
2024-03-13 14:05:49 +0530

    file1

---
+++ file1.py
@@ -0,0 +1,44 @@
+#while loop
+i=0
+while i<10: # statement must be end with scolon
+    print(i)
+    i=i+1
+print ();
+    
+i=0
+while i<10:
+    print(i)
+    if i==5:
+        break
+    i=i+1
+
+print();
+
+j=0
+while j<10:
+    j=j+1
+    if j==5:
+        continue
+    print(j)
+
+
+
+
+for x in "University":
+    print(x)
+    
+print()
+
+for x in range(5):
+    print(x)
+
+print()
+
+# range(a.,b,c) c is increamnt or decremnet value a,b  range of the parameter
+for x in range(1,10,2):
+    print(x)
+
+
+
+
+    
